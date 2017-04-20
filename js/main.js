4ef06d5f2c9b9b826ffabbee65de91cb2161606c
PsychoWalker <online-ss4@yandex.ru>
2017-04-20 11:15:56 +0400

    добавляю анимацию страницы

--- js/main.js
+++ js/main.js
@@ -11,6 +11,34 @@ jQuery(function ($) {
     }
   })
 
+   $(window).scroll(() => {
+    if ($('.prize').is(':in-viewport(1000)')) {
+      $('.prize')
+        .addClass('animation-move-out');
+    }
+  })
+
+   $(window).scroll(() => {
+    if ($('.balloon').is(':in-viewport(1000)')) {
+      $('.balloon')
+        .addClass('animation-move');
+    }
+  })
+
+     $(window).scroll(() => {
+    if ($('.beach').is(':in-viewport(1000)')) {
+      $('.beach')
+        .addClass('animation-move-out');
+    }
+  })
+
+   $(window).scroll(() => {
+    if ($('.summer').is(':in-viewport(1000)')) {
+      $('.summer')
+        .addClass('animation-move');
+    }
+  }) 
+
   $(window).scroll(() => {
     if ($('.beach-image').is(':in-viewport(300)')) {
       $('.beach-image')
